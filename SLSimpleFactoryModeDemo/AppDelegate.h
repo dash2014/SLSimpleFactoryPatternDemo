e6ffd3df72e43c8ae77dade862335ec4f228ab67
史良 <liang.shi@renren-inc.com>
2014-08-08 17:13:00 +0800

    Initial Commit

---
+++ SLSimpleFactoryModeDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SLSimpleFactoryModeDemo
+//
+//  Created by dash on 14-8-8.
+//  Copyright (c) 2014年 com.dash.Qjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SLSimpleFactoryModeDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SLSimpleFactoryModeDemo
+//
+//  Created by dash on 14-8-8.
+//  Copyright (c) 2014年 com.dash.Qjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
